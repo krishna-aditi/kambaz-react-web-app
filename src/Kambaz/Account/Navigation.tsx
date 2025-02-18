75a14f0aeef148cb2e2fbf482fdfe84108766f2e
Aditi Krishna <aditikrishna@Aditis-MacBook-Air.local>
2025-02-18 16:42:36 -0500

    a3 complete

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,9 +1,18 @@
-import { Link } from "react-router-dom";
+import { ListGroup } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 export default function AccountNavigation() {
+  const { pathname } = useLocation();
+  const links = ["Signin", "Signup", "Profile"]
   return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to={`/Kambaz/Account/Signin`} className="list-group-item active border border-0 pb-0"  > Signin  </Link> <br/>
-      <Link to={`/Kambaz/Account/Signup`} className="list-group-item text-danger border border-0 pb-0" > Signup  </Link> <br/>
-      <Link to={`/Kambaz/Account/Profile`} className="list-group-item text-danger border border-0 pb-0 m-0"> Profile </Link> <br/>
-    </div>
+    // <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+    //   <Link to={`/Kambaz/Account/Signin`} className="list-group-item active border border-0 pb-0"  > Signin  </Link> <br/>
+    //   <Link to={`/Kambaz/Account/Signup`} className="list-group-item text-danger border border-0 pb-0" > Signup  </Link> <br/>
+    //   <Link to={`/Kambaz/Account/Profile`} className="list-group-item text-danger border border-0 pb-0 m-0"> Profile </Link> <br/>
+    // </div>
+    <ListGroup id="wd-account-navigation" className="wd fs-5 rounded-0">
+      {links.map((link) => (
+          <Link to={`/Kambaz/Account/${link}`}
+            className={`list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}> {link} </Link>
+      ))}
+    </ListGroup>
 );}
