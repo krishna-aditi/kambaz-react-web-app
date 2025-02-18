f05d93cff0e5204966867555183648429e606ef2
Aditi Krishna <aditikrishna@Aditis-MacBook-Air.local>
2025-02-18 17:57:21 -0500

    a3 complete

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,11 +4,6 @@ export default function AccountNavigation() {
   const { pathname } = useLocation();
   const links = ["Signin", "Signup", "Profile"]
   return (
-    // <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-    //   <Link to={`/Kambaz/Account/Signin`} className="list-group-item active border border-0 pb-0"  > Signin  </Link> <br/>
-    //   <Link to={`/Kambaz/Account/Signup`} className="list-group-item text-danger border border-0 pb-0" > Signup  </Link> <br/>
-    //   <Link to={`/Kambaz/Account/Profile`} className="list-group-item text-danger border border-0 pb-0 m-0"> Profile </Link> <br/>
-    // </div>
     <ListGroup id="wd-account-navigation" className="wd fs-5 rounded-0">
       {links.map((link) => (
           <Link to={`/Kambaz/Account/${link}`}
